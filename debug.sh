56b153b0db596d1f1657adc8adae74530b8981e9
Sasakwa-PC <bshawvt@gmail.com>
2022-06-20 21:14:52 -0400

    ???

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-$BASEDIR/bin/debug/linux/electron $BASEDIR/src/main.js --js-flags='--max_old_space_size=0 --expose-gc' --force_high_performance_gpu -debug $0 $1 $2 $3 $4 $5 $6 $7 $8 $9
+$BASEDIR/bin/debug/linux/electron $BASEDIR/src/main.js --js-flags='--max_old_space_size=0 --expose-gc' --force_high_performance_gpu -debug -x 1440 -y 25 $0 $1 $2 $3 $4 $5 $6 $7 $8 $9
